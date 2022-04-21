77e69e24df7633997069a8953781d413acd79acf
adam-bialy <skrzypeknadachu@gmail.com>
2022-04-21 12:01:10 +0100

    add script and refactor

---
+++ scripts/wakeup.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", function(event) {
+
+  fetch("https://neurvid.herokuapp.com")
+
+  fetch("https://freqexam.herokuapp.com")
+
+  fetch("https://freq-report.herokuapp.com/?name=A&lower=20&upper=15000")
+
+})
