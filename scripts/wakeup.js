c89aefa37b5094e0e3b8ace380d2961274242785
adam-bialy <skrzypeknadachu@gmail.com>
2022-04-21 12:06:25 +0100

    correct script

--- scripts/wakeup.js
+++ scripts/wakeup.js
@@ -2,12 +2,16 @@ var isClicked = false;
 
 document.addEventListener("click", function(event) {
 
+  if (!isClicked) {
+
+    fetch("https://neurvid.herokuapp.com")
+
+    fetch("https://freqexam.herokuapp.com")
+
+    fetch("https://freq-report.herokuapp.com/?name=A&lower=20&upper=15000")
+
+  }
+
   isClicked = true
-
-  fetch("https://neurvid.herokuapp.com")
-
-  fetch("https://freqexam.herokuapp.com")
-
-  fetch("https://freq-report.herokuapp.com/?name=A&lower=20&upper=15000")
 
 });
