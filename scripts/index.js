579de7f09df7e09d7ed58efdc2210292858c65cc
adam-bialy <skrzypeknadachu@gmail.com>
2022-08-05 15:19:20 +0200

    correct script

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,41 @@
 // Collecting anonymous user data.
-let isConsent = false;
-
-function sendInfo(activity) {
+function sendInfoLocale(activity) {
   $.post("https://agstats.herokuapp.com/" + activity, {
     message: activity,
     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
   });
 }
 
+function sendInfoEmpty(activity) {
+  $.post("https://agstats.herokuapp.com/" + activity, {
+    message: activity,
+    timezone: null
+  });
+}
+
 const startTime = performance.now()
 let isScrolled = false;
+let isConsent = false;
 let isRead = false;
+let isViewed = false;
 
 $(".consent-button").on("click", function() {
   if (this.name === "accept") {
     isConsent = true;
-    sendInfo("view")
   }
   $(".consent").remove();
 });
 
 $(window).scroll(function() {
+
+  if (!isViewed) {
+    if (isConsent) {
+      sendInfoLocale("view");
+    } else {
+      sendInfoEmpty("view");
+    }
+    isViewed = true;
+  }
 
   function elementScrolled(elem) {
     var docViewTop = $(window).scrollTop();
@@ -33,8 +48,12 @@ $(window).scroll(function() {
     isScrolled = true
   }
 
-  if (isScrolled & (performance.now() - startTime > 30000) & !isRead & isConsent) {
-    sendInfo("read")
+  if (isScrolled & (performance.now() - startTime > 30000) & !isRead) {
+    if (isConsent) {
+      sendInfoLocale("read");
+    } else {
+      sendInfoEmpty("read");
+    }
     isRead = true;
   }
 });
